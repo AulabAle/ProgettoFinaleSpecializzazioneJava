453933506b603f813727758e063d60bea8e58a5e
Alessandro Leo <alessandro.leo@aulab.it>
2024-07-05 12:49:22 +0200

    Notifiche ok

---
+++ src/main/java/it/aulab/spec_prog_finale/config/NotificationInterceptor.java
@@ -0,0 +1,33 @@
+package it.aulab.spec_prog_finale.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import it.aulab.spec_prog_finale.repositories.ArticleRepository;
+import it.aulab.spec_prog_finale.repositories.CarreerRequestRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Component
+public class NotificationInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @Autowired
+    CarreerRequestRepository carreerRequestRepository;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null) {
+            int revisedCount = articleRepository.findByIsAcceptedFalse().size();
+            int carreerCount = carreerRequestRepository.findByIsCheckedFalse().size();
+            modelAndView.addObject("articlesToBeRevised", revisedCount);
+            modelAndView.addObject("carreerRequests", carreerCount);
+        }
+    }
+}
